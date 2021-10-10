4d2463efef7d5741b1362085074dc336b2680ac5
Johanson Felix <felix.johanson@gmail.com>
2021-10-10 15:38:03 -0400

    first commit

---
+++ src/main/java/com/project/coronavirustracker/models/LocationStats.java
@@ -0,0 +1,51 @@
+package com.project.coronavirustracker.models;
+
+public class LocationStats {
+
+    private String provinceState;
+    private String countryRegion;
+    private int latestTotalCases;
+    private int diffFromPrevDay;
+
+    public int getDiffFromPrevDay() {
+        return diffFromPrevDay;
+    }
+
+    public void setDiffFromPrevDay(int diffFromPrevDay) {
+        this.diffFromPrevDay = diffFromPrevDay;
+    }
+
+
+    public String getProvinceState() {
+        return provinceState;
+    }
+
+    public void setProvinceState(String provinceState) {
+        this.provinceState = provinceState;
+    }
+
+    public String getCountryRegion() {
+        return countryRegion;
+    }
+
+    public void setCountryRegion(String countryRegion) {
+        this.countryRegion = countryRegion;
+    }
+
+    public int getLatestTotalCases() {
+        return latestTotalCases;
+    }
+
+    public void setLatestTotalCases(int latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationStats{" +
+                "provinceState='" + provinceState + '\'' +
+                ", countryRegion='" + countryRegion + '\'' +
+                ", latestTotalCases=" + latestTotalCases +
+                "}\n";
+    }
+}
